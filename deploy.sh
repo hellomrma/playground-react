3ba1e67b6772c69bc72dfae754c769fcb7132143
GwangSeub Ma <hellomrma@gmail.com>
2021-11-08 10:53:51 +0900

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# 오류 발생시 중단한다.
 set -e
 
-# build
+# 문서(md)를 build하여 html로 만든다. 
 yarn docs:build
 
-# navigate into the build output directory
+# build가 output된 폴더로 이동한다. 
 cd .vuepress/dist
 
-# gh-pages clone
-git clone -b gh-pages https://github.com/hellomrma/playground-react/
-cp -rf playground-react/.git ./.git
-rm -rf playground-react
+# init + add + commit을 해준 다음
+git init
+git add -A
+git commit -m 'deploy'
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# https://<USERNAME>.github.io 에 배포하는 경우
+# git push -f https://github.com/<USERNAME>/<USERNAME>.github.io.git master
 
-git add .
-git commit -m "$*"
+# https://<USERNAME>.github.io/<REPO> 에 배포하는 경우
+# git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push origin gh-pages
+# 필자의 경우 TIL repository에 배포하기 때문에 아래와 같이 작성했다.
+git push -f https://github.com/hellomrma/playground-react.git master:gh-pages
 
 cd -
